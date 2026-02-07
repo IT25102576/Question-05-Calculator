c050c9871dc5c4a655c3eb94767612fb4e33c334
IT25102576 <IT25102576@mY.Sliit.lk>
2026-02-07 19:12:37 +0530

    Add files via upload
    LAB 01 - Q 05 (Calculator) (IT25102576 Disanayaka D M M )

---
+++ LAB 01 - Q 05  (IT25102576 Disanayaka D M M ).java
@@ -0,0 +1,30 @@
+public class CalculatorDemo {
+
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    
+    public static int square(int a) {
+        return a * a;
+    }
+
+    
+    public static void main(String[] args) {
+
+        // (3 * 4 + 5 * 7)^2
+        int result1 = square(add(multiply(3, 4), multiply(5, 7)));
+
+        // (4 + 7)^2 + (8 + 3)^2
+        int result2 = add(square(add(4, 7)), square(add(8, 3)));
+
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+    }
+}
